a761908d2b62f97093fdcff9450c76f768657978
euihyuneee <lobil7782@gmail.com>
2022-05-10 19:12:27 +0900

    [Create] 인텔리제이 파일 생성

---
+++ TFT-lolchess/src/main/java/Statisticslol/TFTlolchess/TftLolchessApplication.java
@@ -0,0 +1,13 @@
+package Statisticslol.TFTlolchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftLolchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftLolchessApplication.class, args);
+	}
+
+}
---
+++ TFT-lolchess/src/test/java/Statisticslol/TFTlolchess/TftLolchessApplicationTests.java
@@ -0,0 +1,13 @@
+package Statisticslol.TFTlolchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftLolchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
