a2565bc00f473b40491985386f331bf91aeb1adb
RDDcat <yukmaro@gmail.com>
2022-05-15 15:53:50 +0900

    [ADD] SummonerService 추가

---
+++ tft/src/main/java/com/grassparty/tft/Service/SummonerService.java
@@ -0,0 +1,14 @@
+package com.grassparty.tft.Service;
+
+import com.grassparty.tft.Model.Riot.SummonerDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SummonerService {
+
+    public SummonerDTO GetSummonerDtoByName(){
+        
+        return null;
+    }
+
+}
