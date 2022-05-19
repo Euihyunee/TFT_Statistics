30c20905efa72ff9cc70b4f582e61342c68e743c
RDDcat <yukmaro@gmail.com>
2022-05-20 02:13:05 +0900

    [UPDATE] MatchDTO 업데이트

--- tft/src/main/java/com/grassparty/tft/Model/Riot/MatchDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/Riot/MatchDTO.java
@@ -1,6 +1,12 @@
 package com.grassparty.tft.Model.Riot;
 
+import com.grassparty.tft.Model.Riot.etc.InfoDTO;
+import com.grassparty.tft.Model.Riot.etc.MetadataDTO;
+import lombok.Data;
+
 // riot api DTO 받이
+@Data
 public class MatchDTO {
-    // 이거 너무 큰데 이거 필요할까..
+    InfoDTO infoDTO;
+    MetadataDTO metadataDTO;
 }
--- tft/src/main/java/com/grassparty/tft/Model/Riot/etc/CompanionDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/Riot/etc/CompanionDTO.java
@@ -1,4 +1,5 @@
 package com.grassparty.tft.Model.Riot.etc;
 
 public class CompanionDTO {
+    // 사용처 불분명
 }
