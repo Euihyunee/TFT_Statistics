0b2e62bd5794e6a8c80f8df0c6eea2f420008820
RDDcat <yukmaro@gmail.com>
2022-05-22 17:30:22 +0900

    [FIX] 병합충돌

--- tft/src/main/java/com/grassparty/tft/Model/Riot/etc/UnitDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/Riot/etc/UnitDTO.java
@@ -1,16 +1,10 @@
 package com.grassparty.tft.Model.Riot.etc;
 
-<<<<<<< HEAD
-import lombok.Data;
-
-@Data
-=======
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
->>>>>>> 600b34629d333935af687cd498800df2bc48d077
 public class UnitDTO {
     int[] items;
     String character_id;
