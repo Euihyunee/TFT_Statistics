881170932550e1e26f299208f4e2715c3befd4bf
RDDcat <yukmaro@gmail.com>
2022-05-22 16:42:52 +0900

    [UPDATE] MetaRecord 더미 작동

--- tft/src/main/java/com/grassparty/tft/Controller/Controller.java
+++ tft/src/main/java/com/grassparty/tft/Controller/Controller.java
@@ -1,6 +1,8 @@
 package com.grassparty.tft.Controller;
 
+import com.grassparty.tft.Model.MetaRecordDTO;
 import com.grassparty.tft.Model.Riot.SummonerDTO;
+import com.grassparty.tft.Service.MetaRecordService;
 import com.grassparty.tft.Service.SummonerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,23 +10,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controller {
     @Autowired
-    private SummonerService service = new SummonerService();
+    private SummonerService summonerService = new SummonerService();
+    @Autowired
+    private MetaRecordService metaRecordService = new MetaRecordService();
 
 
     @GetMapping("/")
     public String helloDTO(){ return "hello";}
 
     @GetMapping(path="/puuid/{puuid}")
-    public SummonerDTO GetDTOByPuuid(@PathVariable String puuid){ return service.GetSummonerDtoByPuuid(puuid);}
+    public SummonerDTO GetDTOByPuuid(@PathVariable String puuid){ return summonerService.GetSummonerDtoByPuuid(puuid);}
 
     @GetMapping(path="/name/{name}")
-    public SummonerDTO GetDTOByName(@PathVariable String name){ return service.GetSummonerDTOByName(name);}
+    public SummonerDTO GetDTOByName(@PathVariable String name){ return summonerService.GetSummonerDTOByName(name);}
+
 
     // 문자열 출력 테스트
     @GetMapping(path="/test/{test}")
     public String test(@PathVariable String test){ return test;}
+    // 메타레코드 더미데이터 테스트
+    @GetMapping(path="/test/MetaRecord")
+    public MetaRecordDTO test(){ return metaRecordService.GetDummyDTO();}
     // UTF-8 테스트
     @GetMapping(path="/UTF-8/{name}")
-    public String GetEncodeByName(@PathVariable String name){ return service.GetEncode(name);}
+    public String GetEncodeByName(@PathVariable String name){ return summonerService.GetEncode(name);}
 
 }
--- tft/src/main/java/com/grassparty/tft/Model/MetaTraitDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/MetaTraitDTO.java
@@ -1,6 +1,12 @@
 package com.grassparty.tft.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class MetaTraitDTO{
     String name;
     int style;
+
 }
