c5d6f520f7a47a2fbcf7e52979ab8e99079fba6e
RDDcat <yukmaro@gmail.com>
2022-05-26 02:36:05 +0900

    [ADD] ModelConvertServie와 하위 매서드 추가

--- tft/src/main/java/com/grassparty/tft/Service/MetaRecordService.java
+++ tft/src/main/java/com/grassparty/tft/Service/MetaRecordService.java
@@ -34,6 +34,10 @@ public class MetaRecordService {
         return metaRecordDTOs.get(0);
     }
     public MetaRecordDTO GetMetaRecordDTO(){
+        // FullRecordDTO를 matchid로 DB조회
+        // 있으면 ModelConvertService로 MetaRecordDTO로 변환후 리턴
+        // 없으면 match id로 라이엇에 조회 후 metaRecord 변환 후 리턴 (FullRecordDTO는 DB에 저장)
+
         return metaRecordDTOs.get(0);
     }
 
---
+++ tft/src/main/java/com/grassparty/tft/Service/ModelConvertService.java
@@ -0,0 +1,21 @@
+package com.grassparty.tft.Service;
+
+import com.grassparty.tft.Model.FullRecordDTO;
+import com.grassparty.tft.Model.MetaRecordDTO;
+import com.grassparty.tft.Model.Riot.MatchDto;
+
+public class ModelConvertService {
+
+    public FullRecordDTO GetFullRecordFromMatchDTO(MatchDto matchDto){
+        // Riot DTO인 MatchDTO -> FullRecordDTO 로 변환
+
+        return null;
+    }
+
+    public MetaRecordDTO GetMetaRecordFromFullRecord(FullRecordDTO fullRecordDTO, String puuid){
+        // FullRecordDTO -> MetaRecordDTO 로 변환
+
+        return null;
+    }
+
+}
