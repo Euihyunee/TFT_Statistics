a7015b8e40cd6f2741fad0ab7937711620ec28af
euihyuneee <lobil7782@gmail.com>
2022-05-29 16:49:31 +0900

    [UPDATE] FullRecordDTO 간단 수정

--- tft/src/main/java/com/grassparty/tft/Model/Riot/FullDTO/FullInfoDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/Riot/FullDTO/FullInfoDTO.java
@@ -5,7 +5,13 @@ import lombok.Data;
 
 @Data
 public class FullInfoDTO {
-    // long game_datetime;
+//    long game_datetime;
     float game_length;
+//    String game_variation;
+//    String game_version;
     FullParticipantDTO[] participants;
+//    String tft_game_type;
+//    String tft_set_name;
+//    int queue_id;
+//    int tft_set_number;
 }
--- tft/src/main/java/com/grassparty/tft/Model/Riot/FullDTO/FullMetadataDTO.java
+++ tft/src/main/java/com/grassparty/tft/Model/Riot/FullDTO/FullMetadataDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class FullMetadataDTO {
+//    String data_version;
     String match_id;
     String[] participants;
 }
