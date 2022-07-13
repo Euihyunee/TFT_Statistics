e155ebfda5f301afb4f77e527d29885786d2f370
euihyuneee <lobil7782@gmail.com>
2022-07-14 05:23:07 +0900

    [Release1.0] 전적검색 구현 완료

--- tft/src/main/java/com/grassparty/tft/Controller/TestController.java
+++ tft/src/main/java/com/grassparty/tft/Controller/TestController.java
@@ -33,22 +33,27 @@ public class TestController {
     private FullRecordRepositoryCreate fullRecordRepositoryCreate = new FullRecordRepositoryCreate();
 
 
-
     // 문자열 출력 테스트
-    @GetMapping(path="/{test}")
-    public String test(@PathVariable String test){ return test;}
+    @GetMapping(path = "/{test}")
+    public String test(@PathVariable String test) {
+        return test;
+    }
 
     // 메타레코드 더미데이터 테스트
-    @GetMapping(path="/MetaRecord")
-    public MetaRecordDTO test(){ return metaRecordService.GetDummyDTO();}
+    @GetMapping(path = "/MetaRecord")
+    public MetaRecordDTO test() {
+        return metaRecordService.GetDummyDTO();
+    }
 
     // UTF-8 테스트
-    @GetMapping(path="/UTF-8/{name}")
-    public String GetEncodeByName(@PathVariable String name){ return summonerService.GetEncode(name);}
+    @GetMapping(path = "/UTF-8/{name}")
+    public String GetEncodeByName(@PathVariable String name) {
+        return summonerService.GetEncode(name);
+    }
 
     // GetFullRecordFromMatchDTO test
-    @GetMapping(path="/GetFull")
-    public FullRecordDTO testGetFullRecordFromMatchDTO(){
+    @GetMapping(path = "/GetFull")
+    public FullRecordDTO testGetFullRecordFromMatchDTO() {
         MatchDto matchDto = matchservice.GetMatchDTOByMatchId("KR_5988203332");
 
         return modelConvertService.GetFullRecordFromMatchDTO(matchDto);
@@ -67,6 +72,7 @@ public class TestController {
         // matchID로 matchDTO 받기 x 15
         //MatchDtos matchDtos = matchservice.GetMatchDTOByMatchIds(matchID);
     }
+
     // 작동
     @GetMapping("/test2/{name}")
     public MatchID test2(@PathVariable String name) {
@@ -77,6 +83,7 @@ public class TestController {
         MatchID matchID = matchservice.GetMatchIdByPuuid(summonerDTO.getPuuid());
         return matchID;
     }
+
     // 작동
     @GetMapping("/test3/{name}")
     public MatchDtos test3(@PathVariable String name) {
@@ -91,6 +98,7 @@ public class TestController {
 
         return matchDtos;
     }
+
     // 작동
     @GetMapping("/test4/{name}")
     public FullRecordDTOs test4(@PathVariable String name) {
@@ -110,6 +118,7 @@ public class TestController {
         // FullMatchDTO list 리턴
         return fullRecordDTOs;
     }
+
     // 작동
     @GetMapping("/test5/{name}")
     public MetaRecordDTO test5(@PathVariable String name) {
@@ -132,6 +141,7 @@ public class TestController {
 
         return metaRecordDTO;
     }
+
     // 작동
     @GetMapping("/test6/{name}")
     public MetaRecordDTO test6(@PathVariable String name) {
@@ -179,7 +189,7 @@ public class TestController {
 
     // 전적검색 (작동)
     @GetMapping("/GetMatchHistory/{name}")
-    public FullRecordDTOs testGetMatchHistoryByName(@PathVariable String name){
+    public FullRecordDTOs testGetMatchHistoryByName(@PathVariable String name) {
         // puuid 요청
         SummonerDTO summonerDTO = summonerService.GetSummonerDTOByName(name);
 
@@ -199,26 +209,53 @@ public class TestController {
         // FullRecordDTO list 리턴
         return fullRecordDTOs;
     }
+
+    // IsExistByMatchid
+    // GetFullRecordDTOFromRepository
     // DB insert 작동 확인
     @GetMapping("/Database/{name}")
-    public FullRecordDTO testDBByName(@PathVariable String name){
+    public MetaRecordDTO[] testDBByName(@PathVariable String name) {
         // puuid 요청
         SummonerDTO summonerDTO = summonerService.GetSummonerDTOByName(name);
 
         // matchid 받기
         MatchID matchID = matchservice.GetMatchIdByPuuid(summonerDTO.getPuuid());
 
-        // matchID로 matchDTO 받기 x 15
-        MatchDtos matchDtos = matchservice.GetMatchDTOByMatchIds(matchID);
 
-        // matchDTO FullMatchDTO로 받기
-        FullRecordDTOs fullRecordDTOs;
-        fullRecordDTOs = modelConvertService.GetFullRecordsFromMatchDTOs(matchDtos);
-
-        fullRecordRepositoryCreate.InsertFullRecord(fullRecordDTOs.getFullRecordDTOs()[0]);
+        // FullMatchDTO를 MetaRecordDTO로 변환
+        MetaRecordDTO[] metaRecordDTOs = new MetaRecordDTO[10];
 
 
-        // FullMatchDTO리턴
-        return fullRecordDTOs.getFullRecordDTOs()[0];
+        for(int i=0; i < matchID.getMatchid().length; i++){
+            if (fullRecordRepositoryCreate.IsExistByMatchid(matchID.getMatchid()[i])){
+
+    //             fullRecordDTO를 Matchid로 가져오기(단수)
+                FullRecordDTO fullRecordDTO = fullRecordRepositoryCreate.GetFullRecordDTOFromRepository(matchID.getMatchid()[i]);
+
+                metaRecordDTOs[i] = modelConvertService.GetMetaRecordFromFullRecord(fullRecordDTO, summonerDTO.getPuuid());
+
+            }else{ // matchid가 DB에 없을 떄 경우
+                // matchID로 matchDTO 받기 x 15
+                MatchDto matchDto = matchservice.GetMatchDTOByMatchId(matchID.getMatchid()[i]);
+
+                // matchDTO FullMatchDTO로 받기
+                FullRecordDTO fullRecordDTO;
+                fullRecordDTO = modelConvertService.GetFullRecordFromMatchDTO(matchDto);
+
+                // FullRecord DB 저장
+                fullRecordRepositoryCreate.InsertFullRecord(fullRecordDTO);
+
+                // FullMatchDTO를 MetaRecordDTO로 변환
+                metaRecordDTOs[i] = modelConvertService.GetMetaRecordFromFullRecord(fullRecordDTO, summonerDTO.getPuuid());
+            }
+        }
+        return metaRecordDTOs;
+    }
+
+    @GetMapping("/dbselect/{puuid}")
+    public void testselect(@PathVariable String puuid) {
+
+        fullRecordRepositoryCreate.IsExistByMatchid(puuid);
+
     }
 }
--- tft/src/main/java/com/grassparty/tft/Service/RepositoryService/FullRecordRepositoryCreate.java
+++ tft/src/main/java/com/grassparty/tft/Service/RepositoryService/FullRecordRepositoryCreate.java
@@ -3,11 +3,13 @@ package com.grassparty.tft.Service.RepositoryService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.grassparty.tft.Model.FullRecordDTO;
+import com.grassparty.tft.Model.Riot.MatchID;
 import com.grassparty.tft.Model.domain.FullRecordDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class FullRecordRepositoryCreate {
@@ -33,4 +35,33 @@ public class FullRecordRepositoryCreate {
                 .build();
         fullRecordRepository.save(fullRecordDB);
     }
+
+    public boolean IsExistByMatchid(String matchid){
+
+        return fullRecordRepository.existsById(matchid);
+    }
+
+    public FullRecordDTO[] GetFullRecordDTOsByMatchidFromRepository(MatchID matchID){
+        FullRecordDTO[] fullRecordDTOS = new FullRecordDTO[10];
+        for(int i=0; i<matchID.getMatchid().length;i++){
+            fullRecordDTOS[i] = GetFullRecordDTOFromRepository(matchID.getMatchid()[i]);
+        }
+        return fullRecordDTOS;
+    }
+
+    public FullRecordDTO GetFullRecordDTOFromRepository(String matchid){
+
+        // DB에서 FullRecordDB 가져오기
+        Optional<FullRecordDB> fullRecordDB =  fullRecordRepository.findById(matchid);
+
+        // FullRecordDB에서 json 받아오기
+        String json =  fullRecordDB.get().getJson();
+
+        // Gson 역직렬화까즤
+        Gson gson = new Gson();
+        FullRecordDTO fullRecordDTO = gson.fromJson(json, FullRecordDTO.class);
+
+        return fullRecordDTO;
+    }
+
 }
