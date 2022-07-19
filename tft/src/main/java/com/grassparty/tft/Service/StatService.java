f28d914d80554eff119735001bff146ddbad30ca
euihyuneee <lobil7782@gmail.com>
2022-07-19 20:54:18 +0900

    [ADD] StatService 추가

--- tft/src/main/java/com/grassparty/tft/Service/StatService.java
+++ tft/src/main/java/com/grassparty/tft/Service/StatService.java
@@ -1,7 +1,13 @@
 package com.grassparty.tft.Service;
 
+import com.grassparty.tft.Model.DB.FullRecordDB;
+import com.grassparty.tft.Model.DB.StatValidationTable;
 import com.grassparty.tft.Model.Riot.SummonerDTO;
+import com.grassparty.tft.Service.RepositoryService.FullRecordRepository;
+import com.grassparty.tft.Service.RepositoryService.FullRecordRepositoryCreate;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class StatService {
     @Autowired
@@ -12,6 +18,13 @@ public class StatService {
     private MetaRecordService metaRecordService = new MetaRecordService();
     @Autowired
     private ModelConvertService modelConvertService = new ModelConvertService();
+    @Autowired
+    private FullRecordRepositoryCreate fullRecordRepositoryCreate =
+            new FullRecordRepositoryCreate();
+    @Autowired
+    StatVaildationTableRepository statVaildationTableRepository;
+    @Autowired
+    FullRecordRepository fullRecordRepository;
 
     public void StatisticsByName(String name){
         // 이름을 받아서 puuid 얻음
@@ -29,13 +42,20 @@ public class StatService {
 
     public void StatisticsByMatchId(String matchId){
         // 밸리데이션 체크
+        Optional<StatValidationTable> statValidationTable =
+                statVaildationTableRepository.findById(matchId);
 
         // 기존에 데이터 저장되어있는지 체크
+        boolean statVaild = fullRecordRepository.existsById(
+                statValidationTable.get().getMatchID());
 
         // 저장되어있다면 PASS
-
+        if(statVaild == false){
+            fullRecordRepositoryCreate.InsertFullRecord(
+                    modelConvertService.GetFullRecordFromMatchDTO(
+                    matchservice.GetMatchDTOByMatchId(matchId)));
+        }
         // 저장되어 있지 않다면 ? RIOT 요청
-
 
     }
 
---
+++ tft/src/main/java/com/grassparty/tft/Service/StatVaildationTableRepository.java
@@ -0,0 +1,8 @@
+package com.grassparty.tft.Service;
+
+import com.grassparty.tft.Model.DB.StatValidationTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StarVaildationTableRepository extends JpaRepository<StatValidationTable, String> {
+
+}
