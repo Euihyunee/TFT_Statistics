8f2f2ff4f5aa846f8f7623553ba69c49a3a14e34
RDDcat <yukmaro@gmail.com>
2022-07-19 20:06:15 +0900

    [ADD] 외부로 보낼 JSON데이터 클래스 정의

---
+++ tft/src/main/java/com/grassparty/tft/Model/StatisticDTO.java
@@ -0,0 +1,10 @@
+package com.grassparty.tft.Model;
+
+// 덱 티어 리스트 DTO
+public class StatisticDTO {
+    // 덱 이름, 덱 평균 등수(place/count), 챔피언 이름
+    private String deckName;
+    private float placeRate;
+    private String[] character_id;
+
+}
