e147082c4767f69c852ae424cfa397987bc1532e
euihyuneee <lobil7782@gmail.com>
2022-07-20 03:16:35 +0900

    [UPDATE] ScheduleService 고통분담 완료

--- tft/src/main/java/com/grassparty/tft/Service/ScheduleService.java
+++ tft/src/main/java/com/grassparty/tft/Service/ScheduleService.java
@@ -108,178 +108,178 @@ public class ScheduleService {
                 statTable.setTFT7_Nidalee(1);
                 break;
             case "TFT7_Leona":
-
+                statTable.setTFT7_Leona(1);
                 break;
             case "TFT7_Vladimir":
-
+                statTable.setTFT7_Vladimir(1);
                 break;
             case "TFT7_Senna":
-
+                statTable.setTFT7_Senna(1);
                 break;
             case "TFT7_Sejuani":
-
+                statTable.setTFT7_Sejuani(1);
                 break;
             case "TFT7_Sett":
-
+                statTable.setTFT7_Sett(1);
                 break;
             case "TFT7_Skarner":
-
+                statTable.setTFT7_Skarner(1);
                 break;
             case "TFT7_Aatrox":
-
+                statTable.setTFT7_Aatrox(1);
                 break;
             case "TFT7_Ezreal":
-
+                statTable.setTFT7_Ezreal(1);
                 break;
             case "TFT7_Taric":
-
+                statTable.setTFT7_Taric(1);
                 break;
             case "TFT7_Karma":
-
+                statTable.setTFT7_Karma(1);
                 break;
             case "TFT7_TahmKench":
-
+                statTable.setTFT7_TahmKench(1);
                 break;
             case "TFT7_Heimerdinger":
-
+                statTable.setTFT7_Heimerdinger(1);
                 break;
             case "TFT7_Gnar":
-
+                statTable.setTFT7_Gnar(1);
                 break;
             case "TFT7_Nami":
-
+                statTable.setTFT7_Nami(1);
                 break;
             case "TFT7_Lillia":
-
+                statTable.setTFT7_Lillia(1);
                 break;
             case "TFT7_Braum":
-
+                statTable.setTFT7_Braum(1);
                 break;
             case "TFT7_Shen":
-
+                statTable.setTFT7_Shen(1);
                 break;
             case "TFT7_Thresh":
-
+                statTable.setTFT7_Thresh(1);
                 break;
             case "TFT7_Ashe":
-
+                statTable.setTFT7_Ashe(1);
                 break;
             case "TFT7_Yone":
-
+                statTable.setTFT7_Yone(1);
                 break;
             case "TFT7_Jinx":
-
+                statTable.setTFT7_Jinx(1);
                 break;
             case "TFT7_Kayn":
-
+                statTable.setTFT7_Kayn(1);
                 break;
             case "TFT7_Qiyana":
-
+                statTable.setTFT7_Qiyana(1);
                 break;
             case "TFT7_Tristana":
-
+                statTable.setTFT7_Tristana(1);
                 break;
             case "TFT7_Twitch":
-
+                statTable.setTFT7_Twitch(1);
                 break;
             case "TFT7_Nunu":
-
+                statTable.setTFT7_Nunu(1);
                 break;
             case "TFT7_Diana":
-
+                statTable.setTFT7_Diana(1);
                 break;
             case "TFT7_Ryze":
-
+                statTable.setTFT7_Ryze(1);
                 break;
             case "TFT7_Lulu":
-
+                statTable.setTFT7_Lulu(1);
                 break;
             case "TFT7_LeeSin":
-
+                statTable.setTFT7_LeeSin(1);
                 break;
             case "TFT7_Varus":
-
+                statTable.setTFT7_Varus(1);
                 break;
             case "TFT7_Volibear":
-
+                statTable.setTFT7_Volibear(1);
                 break;
             case "TFT7_Sylas":
-
+                statTable.setTFT7_Sylas(1);
                 break;
             case "TFT7_Swain":
-
+                statTable.setTFT7_Swain(1);
                 break;
             case "TFT7_Anivia":
-
+                statTable.setTFT7_Anivia(1);
                 break;
             case "TFT7_Elise":
-
+                statTable.setTFT7_Elise(1);
                 break;
             case "TFT7_Olaf":
-
+                statTable.setTFT7_Olaf(1);
                 break;
             case "TFT7_Illaoi":
-
+                statTable.setTFT7_Illaoi(1);
                 break;
             case "TFT7_Neeko":
-
+                statTable.setTFT7_Neeko(1);
                 break;
             case "TFT7_Sona":
-
+                statTable.setTFT7_Sona(1);
                 break;
             case "TFT7_Ornn":
-
+                statTable.setTFT7_Ornn(1);
                 break;
             case "TFT7_Xayah":
-
+                statTable.setTFT7_Xayah(1);
                 break;
             case "TFT7_Corki":
-
+                statTable.setTFT7_Corki(1);
                 break;
             case "TFT7_Talon":
-
+                statTable.setTFT7_Talon(1);
                 break;
             case "TFT7_Hecarim":
-
+                statTable.setTFT7_Hecarim(1);
                 break;
             case "TFT7_Bard":
-
+                statTable.setTFT7_Bard(1);
                 break;
             case "TFT7_Soraka":
-
+                statTable.setTFT7_Soraka(1);
                 break;
             case "TFT7_Yasuo":
-
+                statTable.setTFT7_Yasuo(1);
                 break;
             case "TFT7_Zoe":
-
+                statTable.setTFT7_Zoe(1);
                 break;
             case "TFT7_Pyke":
-
+                statTable.setTFT7_Pyke(1);
                 break;
             case "TFT7_DragonBlue":
-
+                statTable.setTFT7_DragonBlue(1);
                 break;
             case "TFT7_DragonGold":
-
+                statTable.setTFT7_DragonGold(1);
                 break;
             case "TFT7_DragonPurple":
-
+                statTable.setTFT7_DragonPurple(1);
                 break;
             case "TFT7_DragonGreen":
-
+                statTable.setTFT7_DragonGreen(1);
                 break;
             case "TFT7_TrainerDragon":
-
+                statTable.setTFT7_TrainerDragon(1);
                 break;
             case "TFT7_AoShin":
-
+                statTable.setTFT7_AoShin(1);
                 break;
             case "TFT7_AurelionSol":
-
+                statTable.setTFT7_AurelionSol(1);
                 break;
             case "TFT7_Shyvana":
-
+                statTable.setTFT7_Shyvana(1);
                 break;
 
 
