b508d1b1cc525a7633db4da324e517616ede8e4f
RDDcat <yukmaro@gmail.com>
2022-07-19 22:42:10 +0900

    [UPDATE] 구조변경

---
+++ tft/src/main/java/com/grassparty/tft/Repository/StatValidationRepository.java
@@ -0,0 +1,31 @@
+package com.grassparty.tft.Repository;
+
+import com.grassparty.tft.Model.DB.StatValidationTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StatValidationRepository {
+    @Autowired StatVaildationTableRepository statVaildationTableRepository;
+
+    // matchId[]를 DB Statvalidation 테이블에서 가져오기
+    public String[] GetMatchIdsFromStatValidation(){
+        // valid가 false인 matchid를 어떻게 가져오지?
+        // String matchId[] =
+        // 가져온 데이터는 True로 업데이트
+
+
+        return null;
+    }
+
+    // matchId[]를 DB Statvalidation 테이블에서 가져오기
+    public String GetMatchIdFromStatValidation(){
+        // valid가 false인 matchid를 어떻게 가져오지?
+        // String matchId[] =
+        // 가져온 데이터는 True로 업데이트
+
+
+        return null;
+    }
+
+}
--- tft/src/main/java/com/grassparty/tft/Service/ScheduleService.java
+++ tft/src/main/java/com/grassparty/tft/Service/ScheduleService.java
@@ -3,6 +3,7 @@ package com.grassparty.tft.Service;
 import com.grassparty.tft.Repository.FullRecordRepository;
 import com.grassparty.tft.Repository.FullRecordRepositoryCreate;
 import com.grassparty.tft.Repository.StatVaildationTableRepository;
+import com.grassparty.tft.Repository.StatValidationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,23 @@ public class ScheduleService {
     StatVaildationTableRepository statVaildationTableRepository;
     @Autowired
     FullRecordRepository fullRecordRepository;
+    @Autowired
+    StatValidationRepository statValidationRepository;
 
     @Scheduled(fixedDelay = 300000)
     public void testMethod2(){
         System.out.println("2번 기능 5분에 한번 실행");
 
-        // matchId[]를 DB Statvalidation 테이블에서 가져오기
-        // valid가 false인 matchid를 어떻게 가져오지?
-        // String matchId[] =
-        // 가져온 데이터는 True로 업데이트
+        // 1000번 실행
+        for(int i=0; i<1000; i++){
+            exec();
+        }
+
+    }
+
+    public void exec(){
+        // matchId를 DB Statvalidation 테이블에서 가져오기
+        String matchId = statValidationRepository.GetMatchIdFromStatValidation();
 
         // DB에서 FullRecordDB 가져오기
 
@@ -32,8 +41,8 @@ public class ScheduleService {
         // FullRecordDTO에서 StatTable로 변환 (*)
 
         // StatTable을 DB에 저장
-
     }
+
 
     @Scheduled(fixedDelay = 300000)
     public void testMethod3(){
