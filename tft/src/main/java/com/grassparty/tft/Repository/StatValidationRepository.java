6b085417b5d81ce47eb176f67cb65d47c32d3ec2
RDDcat <yukmaro@gmail.com>
2022-08-15 21:12:45 +0900

    [DELETE] 벨리데이션 체킹 리포지토리에 구현되어있어서 서비스에서 삭제

--- tft/src/main/java/com/grassparty/tft/Repository/StatValidationRepository.java
+++ tft/src/main/java/com/grassparty/tft/Repository/StatValidationRepository.java
@@ -10,15 +10,6 @@ public class StatValidationRepository {
     @Autowired
     StatVaildationRepositoryJPA statVaildationRepositoryJPA;
 
-
-    // matchId[]를 DB Statvalidation 테이블에서 가져오기
-    public String[] GetMatchIdsFromStatValidation(){
-        // valid가 false인 matchid를 어떻게 가져오지?
-        // 가져온 데이터는 True로 업데이트
-
-        return null;
-    }
-
     // matchId를 DB Statvalidation 테이블에서 가져오기
     public String GetMatchIdFromStatValidation(){
         try {
