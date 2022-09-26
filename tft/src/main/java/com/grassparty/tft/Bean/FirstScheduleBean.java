395533544bbe409b957cd95fc27dfb7be6f1671e
RDDcat <yukmaro@gmail.com>
2022-09-26 20:13:53 +0900

    [대격변] 스케줄러에 있는 매서드들 빈으로 3개 분리

---
+++ tft/src/main/java/com/grassparty/tft/Bean/FirstScheduleBean.java
@@ -0,0 +1,50 @@
+package com.grassparty.tft.Bean;
+
+import com.google.gson.Gson;
+import com.grassparty.tft.History.Repository.FullRecordRepository;
+import com.grassparty.tft.History.Repository.StatValidationRepository;
+import com.grassparty.tft.Model.DB.FullRecordDB;
+import com.grassparty.tft.Model.DTO.FullRecordDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+// 매치아이디 밸리데이션 데이블 돌면서 통계에 들어가지않은 애들 1000개씩 통계에 추가
+public class FirstScheduleBean {
+    @Autowired
+    FullRecordRepository fullRecordRepository;
+    @Autowired
+    StatValidationRepository statValidationRepository;
+
+
+    public void exec(){
+        System.out.println("1번 기능 5분에 한번 실행----------------------------------------");
+        // 1000번 실행
+        for(int i=0; i<1000; i++){
+            // 실행할 매치가 없으면 중단
+            if(!method()){ break; }
+        }
+    }
+    SaveStatTableFromFullRecordDTOBean SaveConvertStatTableFromFullRecordDTOBean = new SaveStatTableFromFullRecordDTOBean();
+
+    private boolean method(){
+        // matchId를 DB Statvalidation 테이블에서 가져오기
+        String matchid = statValidationRepository.GetMatchIdFromStatValidation();
+
+        // DB에서 FullRecordDB 가져오기
+        if(matchid == null){
+            System.out.println("매치아이디 널임");
+            return false;
+        }
+        FullRecordDB fullRecordDB = fullRecordRepository.GetFullRecordDBByMatchId(matchid);
+
+
+        // FullRecordDB에서 FullRecordDTO를 분리
+        Gson gson = new Gson();
+        FullRecordDTO fullRecordDTO = gson.fromJson(fullRecordDB.getJson(), FullRecordDTO.class);
+
+        // FullRecordDTO에서 StatTable로 변환 (*)
+        SaveConvertStatTableFromFullRecordDTOBean.exec(fullRecordDTO);
+        System.out.println("몇회실행됬는지 확인점");
+
+        return true;
+    }
+}
