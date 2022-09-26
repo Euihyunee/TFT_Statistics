ae38629cceb2d1c28c4bc3f4918c0b737eb94ab1
euihyuneee <lobil7782@gmail.com>
2022-09-26 19:17:41 +0900

    [대격변] StatisticsByNameBean 추가 및 이슈: Service @Autowired필요

---
+++ tft/src/main/java/com/grassparty/tft/Bean/StatisticsByNameBean.java
@@ -0,0 +1,18 @@
+package com.grassparty.tft.Bean;
+
+import com.grassparty.tft.History.Service.SummonerService;
+import com.grassparty.tft.Model.Riot.SummonerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StatisticsByNameBean {
+
+    @Autowired
+    SummonerService summonerService;
+
+    public String exec(String name){
+        // 이름을 받아서 puuid 얻음
+        SummonerDTO summonerDTO = summonerService.GetSummonerDTOByName(name);
+
+        return summonerDTO.getPuuid();
+    }
+}
