f8357e01a72b7e12d747dd9e9574916bc2f56d77
RDDcat <yukmaro@gmail.com>
2022-09-27 21:51:57 +0900

    [대격변] History 서비스 빈들 @Autowired

--- tft/src/main/java/com/grassparty/tft/History/Service/HistoryService.java
+++ tft/src/main/java/com/grassparty/tft/History/Service/HistoryService.java
@@ -7,13 +7,18 @@ import com.grassparty.tft.Model.DTO.FullRecordDTO;
 import com.grassparty.tft.Model.DTO.MetaRecordDTO;
 import com.grassparty.tft.Model.Riot.MatchDto;
 import com.grassparty.tft.Model.Riot.MatchID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HistoryService {
-    GetMatchHistoryByNameBean GetMatchHistoryByNameBean = new GetMatchHistoryByNameBean();
+    @Autowired
+    GetMatchHistoryByNameBean GetMatchHistoryByNameBean;
+    @Autowired
     GetRecordByMatchIdBean GetRecordByMatchIdBean = new GetRecordByMatchIdBean();
+    @Autowired
     GetMatchDTOBean GetMatchDTOBean = new GetMatchDTOBean();
+    @Autowired
     GetMatchIDBean GetMatchIDBean = new GetMatchIDBean();
 
 
