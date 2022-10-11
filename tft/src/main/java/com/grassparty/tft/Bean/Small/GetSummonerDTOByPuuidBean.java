de1fa39b5930d5afb679dd1c245e6dc3986443ea
RDDcat <yukmaro@gmail.com>
2022-10-11 20:41:29 +0900

    [대격변] Dto로 되있는 이름 DTO로 변경

--- tft/src/main/java/com/grassparty/tft/Bean/Small/GetSummonerDTOByPuuidBean.java
+++ tft/src/main/java/com/grassparty/tft/Bean/Small/GetSummonerDTOByPuuidBean.java
@@ -2,12 +2,11 @@ package com.grassparty.tft.Bean.Small;
 
 import com.grassparty.tft.Model.Riot.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GetSummonerDtoByPuuidBean {
+public class GetSummonerDTOByPuuidBean {
 
     @Autowired
     GetSummonerDTOBean getSummonerDTOBean;
