49e1361cbede101fdd656c22bc5402c6c60d7a9b
RDDcat <yukmaro@gmail.com>
2022-10-26 23:51:31 +0900

    [UPDATE] api 문서에 따라 컨트롤러 요청 틀 만들어둠

--- tft/src/main/java/com/grassparty/tft/History/Controller/Controller.java
+++ tft/src/main/java/com/grassparty/tft/History/Controller/Controller.java
@@ -8,5 +8,27 @@ public class Controller {
     @GetMapping("/")
     public String helloDTO(){ return "hello";}
 
+    @GetMapping("/user")
+    public String GetUser(){
+        return "user 데이터";
+    }
+
+
+    @GetMapping("/unit")
+    public String GetUnit(){
+        return "unit 데이터";
+    }
+
+
+    @GetMapping("/item")
+    public String GetItem(){
+        return "item 데이터";
+    }
+
+
+    @GetMapping("/champ")
+    public String GetChamp(){
+        return "champ 데이터";
+    }
 
 }
