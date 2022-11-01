7615ffc858e5cc85e31fb3cd5629db97cc37b883
RDDcat <yukmaro@gmail.com>
2022-11-01 20:31:48 +0900

    [UPDATE] 버전 정보 테이블 설계 변경에따른 테이블 변경

--- tft/src/main/java/com/grassparty/tft/Model/DAO/VersionDAO.java
+++ tft/src/main/java/com/grassparty/tft/Model/DAO/VersionDAO.java
@@ -1,23 +1,23 @@
 package com.grassparty.tft.Model.DAO;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "version_season")
-public class VersionSeasonDAO {
+@Table(name = "version")
+public class VersionDAO {
 
     @Id
     private Long season_id;
 
     @Column
     private int season_version;
+    @Column
+    private int update_version;
+    @Column
+    private float total_version;
+    
 }
