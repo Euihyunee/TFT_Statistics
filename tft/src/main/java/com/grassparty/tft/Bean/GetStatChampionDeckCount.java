d8bed676cf443afd6c14b900d4fd068052f3f568
euihyuneee <lobil7782@gmail.com>
2022-11-04 01:12:31 +0900

    [UPDATE] GetVersionBean 수정

---
+++ tft/src/main/java/com/grassparty/tft/Bean/GetStatChampionDeckCount.java
@@ -0,0 +1,12 @@
+package com.grassparty.tft.Bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetStatChampionDeckCount {
+    /*
+    version_id : version을 보내면서 version_id 가져옴 version_id가
+    없다면 새로 생성 total_deck_count = 0으로 초기값
+    total_deck_count : version_id로 찾아서 +8하기
+     */
+}
--- tft/src/main/java/com/grassparty/tft/Bean/Small/GetVersionBean.java
+++ tft/src/main/java/com/grassparty/tft/Bean/Small/GetVersionBean.java
@@ -1,24 +1,44 @@
 package com.grassparty.tft.Bean.Small;
 
+import com.grassparty.tft.Model.DAO.RecordDAO;
+import com.grassparty.tft.Model.DAO.VersionDAO;
+import com.grassparty.tft.Model.DTO.RecordDTO;
+import com.grassparty.tft.Model.Riot.MatchID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class GetVersionBean {
+
+    @Autowired
+    GetRecordBean getRecordBean;
+
+    @Autowired
+    GetMatchIdFromStatValidBean getMatchIdFromStatValidBean;
+
     public void exec(){
         // DB record 테이블에서 json(RecordDTO) 가져오기
+        MatchID matchID = getMatchIdFromStatValidBean.exec();
 
-
+        List<RecordDTO> records = getRecordBean.exec(matchID);
 
         // json데이터에서 game_version 가져오기
         // ex) 12.20.474.8882
+        for (RecordDTO recordDTO : records){
 
+            // parsing 부분 Bean으로 만들거 시발~~~~~
+            // gmae_version parsing해서 12는 season_id, 20은 update_id로 나머지는 total_id로 분리
+            String GameVersion = recordDTO.getGame_version();
+            String[] GameVersionSplit = GameVersion.split("\\.");
+            int SeasonId = Integer.parseInt(GameVersionSplit[0]);
+            int updateId = Integer.parseInt(GameVersionSplit[1]);
+            String TotalId = GameVersion;
 
+            // version_table에 조회해서 있으면 냅두고 없으면 추가하는 방식 만들기
 
-        // gmae_version parsing해서 12는 season_id, 20은 update_id로 나머지는 etc?로 분리
+        }
 
-
-
-
-        // version_table에 조회해서 있으면 냅두고 없으면 추가하는 방식 만들기
     }
 }
