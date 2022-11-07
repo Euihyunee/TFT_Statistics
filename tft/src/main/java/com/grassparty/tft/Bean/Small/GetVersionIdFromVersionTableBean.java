d63be04029bba355cc8d9481bdabf7f00e64c341
euihyuneee <lobil7782@gmail.com>
2022-11-07 22:06:14 +0900

    [ADD] version테이블에서 versionId(Long값) 가져오는 Bean

--- tft/src/main/java/com/grassparty/tft/Bean/Small/GetVersionIdFromVersionTableBean.java
+++ tft/src/main/java/com/grassparty/tft/Bean/Small/GetVersionIdFromVersionTableBean.java
@@ -1,9 +1,18 @@
 package com.grassparty.tft.Bean.Small;
 
+import com.grassparty.tft.Model.DAO.VersionDAO;
+import com.grassparty.tft.Repository.JPA.VersionRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GetVersionIdFromVersionTableBean {
 
+    @Autowired
+    VersionRepositoryJPA versionRepository;
     // version_id를 version테이블에서 가져오기
+    public Long exec(String totalVersion){
+        VersionDAO versionDAO = versionRepository.findByTotalVersion(totalVersion);
+        return versionDAO.getVersionId();
+    }
 }
